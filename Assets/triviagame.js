f1fc7844f460ee10680536eeb45870f784470172
Helen Pitt <helenpitt@Helens-MacBook-Pro.local>
2017-10-22 21:07:52 -0400

    verrr basic functionality

--- Assets/triviagame.js
+++ Assets/triviagame.js
@@ -6,9 +6,19 @@ var rightAnswers = 0;
 
 var wrongAnswers = 0;
 
+//With the timer function
+
+
+
+
+
+
+
+
 $(document).ready(function () {
 
-            //initialize the game with hidden divs.    
+//Start Button is showing.    
+//initialize the game with hidden divs.    
             $(".timer_container").hide();
             $(".score_container").hide();
             $(".q1_container").hide();
@@ -16,12 +26,35 @@ $(document).ready(function () {
             $(".q3_container").hide();
 
             $("#startButton").on("click", function () {
+//Q1
+//On click start triggers the timer
 
                 $("#startcontainer").hide();
                 $(".timer_container").show();
                 $(".q1_container").show();
+
+
+        $(".btn").on("click", function (){
+  //Q2
+  //On click resets te timer
+
+            $("#startcontainer").hide();
+            $(".timer_container").show();
+            $(".q1_container").hide();
+            $(".q2_container").show();
+
+            $(".btn").on("click", function(){
+                
+
+                $("#startcontainer").hide();
+                $(".timer_container").show();
                 $(".q2_container").hide();
-                $(".q3_container").hide();
+                $(".q3_container").show();
+                
+            })
+        })
+
+
             })
         
         
