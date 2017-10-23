a9c4a81424f5aa2d1b7ad1518e06456ae832168e
Helen Pitt <helenpitt@Helens-MacBook-Pro.local>
2017-10-22 21:24:04 -0400

    it moves

--- Assets/triviagame.js
+++ Assets/triviagame.js
@@ -17,47 +17,56 @@ var wrongAnswers = 0;
 
 $(document).ready(function () {
 
-//Start Button is showing.    
-//initialize the game with hidden divs.    
-            $(".timer_container").hide();
-            $(".score_container").hide();
-            $(".q1_container").hide();
-            $(".q2_container").hide();
-            $(".q3_container").hide();
+    //Start Button is showing.    
+    //initialize the game with hidden divs.    
+    $(".timer_container").hide();
+    $(".score_container").hide();
+    $(".q1_container").hide();
+    $(".q2_container").hide();
+    $(".q3_container").hide();
 
-            $("#startButton").on("click", function () {
-//Q1
-//On click start triggers the timer
+    $("#startButton").on("click", function () {
+        //Q1
+        //On click start triggers the timer
 
-                $("#startcontainer").hide();
-                $(".timer_container").show();
-                $(".q1_container").show();
+        $("#startcontainer").hide();
+        $(".timer_container").show();
+        $(".q1_container").show();
 
 
-        $(".btn").on("click", function (){
-  //Q2
-  //On click resets te timer
+        $(".btn").on("click", function () {
+            //Q2
+            //On click resets the timer
 
-            $("#startcontainer").hide();
-            $(".timer_container").show();
             $(".q1_container").hide();
             $(".q2_container").show();
 
-            $(".btn").on("click", function(){
-                
+            $(".btn").on("click", function () {
+                //Q3
+                //On click resets the timer
 
-                $("#startcontainer").hide();
-                $(".timer_container").show();
                 $(".q2_container").hide();
                 $(".q3_container").show();
-                
+
+                $(".btn").on("click", function () {
+                    //score_container
+                    //Timer's done
+                    //Want to try again brings you to the start button.
+                    
+                    $(".timer_container").hide();
+                    $(".q3_container").hide();
+                    $(".score_container").show();
+
+                    
+                })
+
             })
         })
 
 
-            })
-        
-        
-        
-        //Ending function for doc.ready
-        })
+    })
+
+
+
+    //Ending function for doc.ready
+})
